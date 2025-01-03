a30e6d1c4b22f28f7cc48cfedeffbe7892ac6095
MD.NASIR UDDIN ANIK <nasiruddinanik54@gmail.com>
2025-01-03 20:52:42 +0600

    four complete

--- bistro-boss-client/src/Layout/Dashboard.jsx
+++ bistro-boss-client/src/Layout/Dashboard.jsx
@@ -1,10 +1,11 @@
-import { Rating } from "@smastrom/react-rating";
-import { FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { FaAccusoft } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
+import UseCart from "../hooks/UseCart";
 
 
 const Dashboard = () => {
+    const [cart] = UseCart()
     return (
         <div className="flex">
             {/* dashboard side bar */}
@@ -12,13 +13,16 @@ const Dashboard = () => {
                 <ul className="menu">
                     <li><NavLink to="/dashboard/userHome"><FaHome></FaHome>User Home</NavLink></li>
                     <li><NavLink to="/dashboard/reservation"><FaCalendar></FaCalendar>Reservation</NavLink></li>
-                    <li><NavLink to="/dashboard/cart"><FaShoppingCart />My Cart</NavLink></li>
+                    <li><NavLink to="/dashboard/cart"><FaShoppingCart />My Cart ({cart.length})</NavLink></li>
                     <li><NavLink to="/dashboard/review"><FaAccusoft></FaAccusoft>Add a Review</NavLink></li>
                     <li><NavLink to="/dashboard/bookings"><FaList></FaList>My Bookings</NavLink></li>
+                    <div className="divider"></div>
+                    <li><NavLink to="/"><FaHome></FaHome>Home</NavLink></li>
+                    <li><NavLink to="/order/salad"><FaSearch></FaSearch>Home</NavLink></li>
                 </ul>
             </div>
             {/* navigation */}
-            <div className="flex-1">
+            <div className="flex-1 p-8">
                 <Outlet></Outlet>
             </div>
         </div>
--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.faeap.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,14 @@ async function run() {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem)
       res.send(result)
+    })
+
+    // cart delete
+     app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
